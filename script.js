940f59b0f4b70edfb119bd604bbd15b3c4438066
jshgyn <josh@joshguyan.com>
2014-02-18 18:02:39 +0000

    code tidying

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 $( document ).ready(function() {
 
+// -------- MENU --------------------------------
+
+	// show menu
+	$("#show_menu").click(function() {
+		$("#show_menu").animate({width:"0px"}, 100, function() {
+			$(".left_section").show();
+			$(".left_section").animate({width: "20%"}, 100);
+		});
+	});
+
+	// hide menu
+	$("#hide_menu").click(function() {
+		$(".left_section").animate({width: "0%"}, 100, function() {
+			$(".left_section").hide();
+			$("#show_menu").animate({width:"20px"}, 100);
+		});
+	});
+
+	// menu item actions
+	$("#new_pedal").click(function() {	
+		addPedal();
+	});
+
+// -------- PEDAL --------------------------------
+
+	// pedal movement
 	$(function() {
 	    $( ".pedal" ).draggable({ containment: ".bottom_section", scroll: false });
 	 });
 
-	 $("#hide_menu").click(function() {
-	 	$(".left_section").animate({width: "0%"}, 100, function() {
-	 		$(".left_section").hide();
-	 		$("#show_menu").animate({width:"20px"}, 100);
-	 	});
-	 });
-
-	 $("#show_menu").click(function() {
-	 	$("#show_menu").animate({width:"0px"}, 100, function() {
-	 		$(".left_section").show();
-	 		$(".left_section").animate({width: "20%"}, 100);
-	 	});
-	 });
-
-	 $("#new_pedal").click(function() {	
-	 	$.get("pedal.html", function(data){
-		    $(".pedal_section").append(data);
-		   	$( ".pedal" ).draggable({ containment: ".bottom_section", scroll: false });
+	// add pedal
+	function addPedal () {	
+		$.get("pedal.html", function(data){
+	    	$(".pedal_section").append(data);
+	   		$( ".pedal" ).draggable({ containment: ".bottom_section", scroll: false });
 		}); 
-	 });
+	}
 
+// -------- END --------------------------------
 });
